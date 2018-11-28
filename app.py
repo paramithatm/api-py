24a66cd6e56056e2346a67e76e0a9222cc767cfb
Paramitha <paramithatm@gmail.com>
2018-11-28 20:58:43 +0700

    get and post request

--- app.py
+++ app.py
@@ -1,3 +1,57 @@
-import flask-restful
-import pymongo
+from flask import Flask, jsonify
+from flask_restful import Api, Resource, reqparse
+from pymongo import MongoClient
+import json
+from bson import ObjectId
 
+# connect to mongodb
+client = MongoClient('mongodb://admin:adm00n@test-cluster-shard-00-00-zk4lm.mongodb.net:27017,test-cluster-shard-00-01-zk4lm.mongodb.net:27017,test-cluster-shard-00-02-zk4lm.mongodb.net:27017/test?ssl=true&replicaSet=test-cluster-shard-0&authSource=admin&retryWrites=true')
+
+db = client['crud']
+coll = db['item']
+
+# provide context to flask
+app = Flask(__name__)
+
+# json encoder for objectId mongodb
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        else:
+            pass
+        return json.JSONEncoder.default(self, o)
+
+# endpoint /
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/get', methods=['GET'])
+def get_item():
+    res = []
+    for x in coll.find():
+        res.append(x)
+    
+    #jsonify the encoded result
+    return jsonify({'items': json.loads(JSONEncoder().encode(res))}), 200
+
+@app.route('/post', methods=['POST'])
+def post_item():
+    parser = reqparse.RequestParser()
+    parser.add_argument('name')
+    parser.add_argument('age')
+    parser.add_argument('detail')
+    args = parser.parse_args()
+    
+    obj = {
+        'name': args['name'],
+        'age': args['age'],
+        'detail': args['detail']
+    }
+    coll.insert_one(obj)
+    return 'item posted successfully', 201
+
+# run the backend
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
